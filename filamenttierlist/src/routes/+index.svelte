6b93766efadb5ff73ce5e72cf91210ff2e872e74
Daniel Gombar <daniel.gombar@sap.com>
2024-07-23 20:41:30 +0200

    Adjust project structure and Add basic files

---
+++ filamenttierlist/src/routes/+index.svelte
@@ -0,0 +1,8 @@
+<script>
+import TierList from './components/TierList.svelte';
+import FilamentItem from './components/FilamentItem.svelte';
+import FilamentDetails from './components/FilamentDetails.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
