15fdfe8d7aeba2f1be574c17008902506b9c8b18
Daniel Gombar <daniel.gombar@sap.com>
2024-07-24 00:18:38 +0200

    Add Close Event in detail page

--- filamenttierlist/src/routes/components/FilamentDetails.svelte
+++ filamenttierlist/src/routes/components/FilamentDetails.svelte
@@ -1,32 +1,50 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte';
     import type { Filament } from '$lib/interfaces';
-  
+
     export let filament: Filament | null = null;
-    export let on: (event: 'close') => void;
+
+    const dispatch = createEventDispatcher();
+
+    function handleClose() {
+        dispatch('close');
+    }
 </script>
 
 {#if filament}
 <div class="details-popup">
     <h2>{filament.name}</h2>
-    <img src="{filament.image}" alt="{filament.name}" />
+    {#if filament.image}
+        <img
+        src={filament.image.src}
+        alt={filament.name}
+        loading="lazy"
+        on:error={(event) => {
+            if (event.target instanceof HTMLImageElement) { 
+                event.target.style.display = 'none';
+            }
+            console.error('Error loading image:', filament.image);
+        }}
+        />
+    {/if}    
+    <!-- TODO: fallback image -->
     <p>{filament.description}</p>
     <h3>Material</h3>
     <p>{filament.material.type} ({filament.material.color})</p>
     <h3>Pros</h3>
     <ul>
-    {#each filament.pros as pro}
-        <li>{pro}</li>
-    {/each}
+        {#each filament.pros as pro}
+            <li>{pro}</li>
+        {/each}
     </ul>
     <h3>Cons</h3>
     <ul>
-    {#each filament.cons as con}
-        <li>{con}</li>
-    {/each}
+        {#each filament.cons as con}
+            <li>{con}</li>
+        {/each}
     </ul>
     <h3>Use Case</h3>
     <p>{filament.useCase}</p>
-    <button on:click={() => on('close')}>Close</button>
+    <button on:click={handleClose}>Close</button>
 </div>
 {/if}
-  
